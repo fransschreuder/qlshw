763e2ea8c0ae524872cab48d11a60addcb81425a
Frans Schreuder <info@schreuderelectronics.com>
2016-12-07 14:45:12 +0100

    fixed src-pkg.sh

---
+++ src-pkg.sh
@@ -0,0 +1,20 @@
+DISTS="trusty xenial yakkety"
+#cp debian-src/* debian
+RELEASE=1.1
+git clone . ../qlshw-$RELEASE
+cd ..
+cd qlshw-$RELEASE
+rm -rf .git
+cd ..
+tar -zcvf qlshw-$RELEASE.orig.tar.gz qlshw-$RELEASE
+cd qlshw-$RELEASE
+COUNT=0
+for DIST in ${DISTS} ; do
+	COUNT=$(($COUNT-1))
+	
+	dch -D $DIST -m -v $RELEASE$COUNT -b
+	
+	debuild -S -k8AD5905E
+	
+	
+done
